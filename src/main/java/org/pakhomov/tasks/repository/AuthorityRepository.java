142eedcdabf767772846f40e17ac48dced6a1b30
Victor Pakhomov <1146089+fossil@users.noreply.github.com>
2018-08-18 16:02:31 +0200

    Initial application generated by JHipster-5.2.1

---
+++ src/main/java/org/pakhomov/tasks/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package org.pakhomov.tasks.repository;
+
+import org.pakhomov.tasks.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
